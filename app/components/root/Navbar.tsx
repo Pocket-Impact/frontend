657e4e94da3299c52c703d70458234af42688c31
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-06 00:06:45 +0200

    ADDED: Link responsiveness

--- app/components/root/Navbar.tsx
+++ app/components/root/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import PrimaryButton from '../ui/PrimaryButton'
 import { links } from '../../lib/links'
@@ -7,6 +8,8 @@ import Image from 'next/image'
 import { CiMenuFries } from "react-icons/ci";
 
 const Navbar = () => {
+    const [open, setOpen] = React.useState(false);
+
     return (
         <>
             <div className='px-4 md:flex hidden bg-background rounded-b-2xl fixed w-full max-w-[1440px] justify-between inter h-16 items-center p-2 left-1/2 top-0 -translate-x-[50%] z-10'>
@@ -27,22 +30,24 @@ const Navbar = () => {
                     <PrimaryButton text="Get Started" styles="text-sm py-3 px-6" />
                 </div>
             </div>
-            <div className='max-md:flex hidden px-4 bg-background rounded-b-2xl fixed w-full max-w-[1440px] justify-between inter h-16 items-center p-2 left-1/2 top-0 -translate-x-[50%] z-10'>
-                <div className='flex items-center gap-2'>
-                    <Image src={logo} alt="Pocket Impact Logo" width={logo.width} height={logo.height} className='w-8 h-8' />
-                    <span className='bricolage xl'>
-                        Pocket Impact
-                    </span>
+            <div className={`max-md:flex flex-col hidden px-4 bg-background z-20 rounded-b-2xl pb-5 outline-black/30 fixed w-full max-w-[1440px] justify-between inter transition-all duration-300 overflow-hidden ${!open ? "h-16" : "h-56 outline"} items-center p-2 left-1/2 top-0 -translate-x-[50%] z-10`}>
+                <div className='flex items-center justify-between w-full'>
+                    <div className='flex items-center gap-2'>
+                        <Image src={logo} alt="Pocket Impact Logo" width={logo.width} height={logo.height} className='w-8 h-8' />
+                        <span className='bricolage xl'>
+                            Pocket Impact
+                        </span>
+                    </div>
+                    <div>
+                        <PrimaryButton onClick={() => setOpen(!open)} icon={<CiMenuFries />} styles="text-sm p-3" />
+                    </div>
                 </div>
-                {/* <div className='flex gap-10 bricolage'>
+                <div className='flex flex-col mt-4 gap-3 bricolage w-full'>
                     {links.map((link) => (
-                        <Link href={link.href} key={link.name} className=''>
+                        <Link href={link.href} key={link.name} className='p-2 w-full'>
                             {link.name}
                         </Link>
                     ))}
-                </div> */}
-                <div>
-                    <PrimaryButton icon={<CiMenuFries />} styles="text-sm p-3" />
                 </div>
             </div>
         </>

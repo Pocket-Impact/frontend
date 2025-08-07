4f686dfb5ca3eb81e225680c29580f57ce97c275
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-07 23:11:23 +0200

    FIXED: Dashboard active link hover state

--- components/dashboard/SideLink.tsx
+++ components/dashboard/SideLink.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const SideLink: React.FC<{ link: any, active: boolean }> = ({ link, active }) => {
     return (
         <Link href={link.href}>
-            <div className={`${active ? "bg-primary text-white" : ""} flex max-md:w-max items-center hover:bg-primary/10 transition duration-300 text-primary gap-3.5 inter font-semibold p-3.5 max-lg:p-3 max-md:p-2 rounded-gl`}>
+            <div className={`${active ? "bg-primary text-white" : "hover:bg-primary/10"} flex max-md:w-max items-center transition duration-300 text-primary gap-3.5 inter font-semibold p-3.5 max-lg:p-3 max-md:p-2 rounded-gl`}>
                 <link.icon className='w-6 max-lg:w-5 max-md:w-4 h-auto' />
                 <span className='base max-md:hidden'>{link.name}</span>
             </div>

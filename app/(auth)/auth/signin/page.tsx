948eaeeda6cc9cddad0aa0ca0927a474ec88ad23
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-09 08:55:56 +0200

    ADDED: Redirect to dashboard if logged in on signin page

--- app/(auth)/auth/signin/page.tsx
+++ app/(auth)/auth/signin/page.tsx
@@ -1,9 +1,15 @@
 import SigninForm from '@/components/auth/SigninForm'
 import React from 'react'
-import logo from '@/public/img/icon.svg'
-import Image from 'next/image'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+    const cookieStore = await cookies()
+    const cookie = cookieStore.get('accessToken')
+
+    if (cookie) {
+        redirect('/feedback/dashboard')
+    }
     return (
         <div className='flex flex-col h-screen'>
             <SigninForm />

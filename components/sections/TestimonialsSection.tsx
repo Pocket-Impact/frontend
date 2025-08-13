2df3759d52e4de3fdb43660a1d68a1dc84cb1fc4
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-13 17:47:56 +0200

    MOVED: Testimonials data into lib folder

--- components/sections/TestimonialsSection.tsx
+++ components/sections/TestimonialsSection.tsx
@@ -1,39 +1,7 @@
 import React from 'react'
 import { MdOutlineStar, MdOutlineStarBorder } from 'react-icons/md'
 import Testimonial from '../root/Testimonial'
-
-const testimonials = [
-  {
-    rating: 5,
-    comment: "Pocket Impact made our reporting seamless and transparent. Highly recommended for any NGO!",
-    author: "Hope Foundation",
-    authorPosition: "Program Manager"
-  },
-  {
-    rating: 4,
-    comment: "The analytics dashboard helped us visualize our progress and engage more donors.",
-    author: "GreenEarth Initiative",
-    authorPosition: "Director"
-  },
-  {
-    rating: 5,
-    comment: "We love the collaboration tools. Our team is more connected than ever.",
-    author: "Bright Future",
-    authorPosition: "Volunteer Coordinator"
-  },
-  {
-    rating: 4,
-    comment: "Pocket Impact has transformed the way we manage our projects. The impact tracking feature is a game-changer!",
-    author: "TechWise",
-    authorPosition: "CEO"
-  },
-  {
-    rating: 5,
-    comment: "Easy to use and very effective for monitoring our field activities.",
-    author: "WaterWorks",
-    authorPosition: "Field Officer"
-  }
-]
+import {testimonials} from '@/lib/testimonials'
 
 const TestimonialsSection = () => {
   return (

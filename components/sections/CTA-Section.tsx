ede1053ae777b2dda017e58cccb423dce20d3765
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-13 14:04:41 +0200

    UPDATED: CTA Section. Added section and button text

--- components/sections/CTA-Section.tsx
+++ components/sections/CTA-Section.tsx
@@ -5,11 +5,11 @@ const CTASection = () => {
     return (
         <div className='p-20 max-lg:p-16 max-md:p-12 max-sm:p-8 flex flex-col items-center rounded-2xl'>
             <div className='flex flex-col items-center text-primary bricolage'>
-                <h1 className='x7l font-bold text-center'>Ready to scale your impact ?</h1>
+                <h1 className='x7l font-bold text-center'>Ready to Do More Good, Faster ?</h1>
                 <p className='mt-4 text-black/70 text-center base'>Join hundreds of nonprofits already using Pocket Impact to do more good, faster.</p>
             </div>
             <SecondaryButton
-                text="Get Started"
+                text="Start Free Trial"
                 styles="text-sm w-max rounded-xl py-3 px-6 mt-8"
             />
         </div>
--- components/sections/FeaturesSection.tsx
+++ components/sections/FeaturesSection.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { features, tools } from '@/lib/tools'
-import { CiCircleCheck } from "react-icons/ci";
 import Feature from '@/components/root/Feature';
 
 const FeaturesSection = () => {
     return (
         <div id='features' className='h-screen scroll-mt-32 mt-16 max-lg:h-max max-md:pt-16 px-24 max-lg:p-8 max-md:p-6 max-sm:p-4 flex flex-col items-center text-black'>
             <div className='text-center'>
-                <h1 className='font-bold bricolage x5l mb-4'>Our features and one <br /> more word to put here</h1>
+                <h1 className='font-bold bricolage x5l mb-4'>Everything You Need in <br /> One Pocket</h1>
                 <p className='text-black/70 inter base'>These are the tools we are going to that we have i.e I need better content.</p>
             </div>
             <div className='grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-4 mt-16 w-full max-w-5xl'>

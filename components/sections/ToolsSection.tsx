acf6cb81ebf3840c967eefd40e76b3f683762654
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-14 11:05:11 +0200

    ADDED: Tool deep dive section

--- components/sections/ToolsSection.tsx
+++ components/sections/ToolsSection.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { IoMdCheckmark } from 'react-icons/io'
+import { tools } from '@/lib/tools'
+import Tool from '@/components/root/Tool'
 
 const ToolsSection = () => {
     return (
-        <div className='py-16'>
-            ToolsSection
+        <div className='py-16 px-28 max-lg:px-20 max-md:px-12 max-sm:px-4 flex flex-col gap-10 inter'>
+            {tools.map((tool, index) => (
+                <Tool tool={tool} index={index} key={tool.name} />
+            ))}
         </div>
     )
 }

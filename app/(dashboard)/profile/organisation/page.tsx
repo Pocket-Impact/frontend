6383bdf174ffba90810fc7eeb0faccbec8e637f0
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-22 21:47:43 +0200

    ADDED: Logout button and functionality

--- app/(dashboard)/profile/organisation/page.tsx
+++ app/(dashboard)/profile/organisation/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import OverviewCard from '@/components/feedback/surveys/OverviewCard'
+import PrimaryButton from '@/components/ui/PrimaryButton';
+import { apiFetch } from '@/utils/apiFetch';
+import { redirect } from 'next/navigation';
 import React from 'react'
 import { BiSearch, BiUser } from 'react-icons/bi';
-import { RiSurveyLine } from 'react-icons/ri';
-import { VscFeedback } from 'react-icons/vsc';
+import { LuLogOut } from 'react-icons/lu';
 
 const overviewCards: any = [
     {
@@ -28,11 +30,24 @@ const overviewCards: any = [
 
 
 const page = () => {
+    const handleClick = () => {
+        const logout = async () => {
+            const response = await apiFetch("/api/auth/logout")
+            redirect('/auth/signin')
+        }
+
+        logout()
+    }
     return (
         <div className='inter flex flex-col gap-6'>
-            <div className=''>
-                <h1 className='x2l font-semibold'>Organisation profile</h1>
-                <p className='text-black/60 base'>Manage organisation.</p>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <h1 className='x2l font-semibold'>Organisation profile</h1>
+                    <p className='text-black/60 base'>Manage organisation.</p>
+                </div>
+                <div>
+                    <PrimaryButton onClick={handleClick} text='Log out' styles='p-3 rounded-lg' icon={<LuLogOut  className='rotate-180' />} />
+                </div>
             </div>
             <div className='grid h-[400px] gap-6 grid-cols-5'>
                 <div className='bg-white col-span-2 flex flex-col gap-4 border rounded-lg p-4 border-stroke'>
@@ -53,7 +68,7 @@ const page = () => {
                         <span className='base'>Medium (20 - 100)</span>
                     </div>
                 </div>
-                <div></div>
+                <div className='bg-white col-span-3 rounded-lg border border-stroke'></div>
             </div>
             <div className='grid grid-cols-3 gap-6'>
                 {overviewCards.map((card: any, index: number) => (

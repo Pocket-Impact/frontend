33eb5c48b4c127ce46506ee877fe1880a17839f6
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-24 21:46:40 +0200

    ADDED: The base analytics page

--- app/(dashboard)/feedback/analytics/page.tsx
+++ app/(dashboard)/feedback/analytics/page.tsx
@@ -1,8 +1,32 @@
+"use client"
+import InfoGrid from '@/components/feedback/InfoGrid'
+import OverviewGrid from '@/components/feedback/OverviewGrid'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='h-full'>
+      <div className='flex flex-col h-full gap-4'>
+        <div className='flex items-start justify-between'>
+          <div className=''>
+            <h1 className='x2l font-semibold'>Analytics</h1>
+            <p className='text-black/60 base'>Welcome to the dashboard of your organisation.</p>
+          </div>
+          <div>
+            <form action="" className='flex items-center gap-3'>
+              <input type="date" className='bg-white p-2 border outline-0 rounded-lg focus:border-primary border-stroke' name="" id="" />
+              <span>to</span>
+              <input type="date" className='bg-white p-2 border outline-0 rounded-lg focus:border-primary border-stroke' name="" id="" />
+            </form>
+          </div>
+        </div>
+        <div className='flex flex-col h-full gap-6'>
+          <OverviewGrid />
+          <InfoGrid />
+        </div>
+      </div>
+      <br /><br />
+    </div>
   )
 }
 

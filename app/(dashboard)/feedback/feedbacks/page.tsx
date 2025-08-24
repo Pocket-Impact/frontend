0dddf7375bddbc6214cf925ec39fec62666a4b78
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-24 21:52:51 +0200

    ADDED: Dummy data

--- app/(dashboard)/feedback/feedbacks/page.tsx
+++ app/(dashboard)/feedback/feedbacks/page.tsx
@@ -2,6 +2,17 @@ import FeedbackCard from '@/components/feedback/feedbacks/FeedbackCard'
 import SurveyCard from '@/components/feedback/surveys/SurveyCard'
 import React from 'react'
 
+const feedbacks = [
+  {
+    category: "Tech",
+    content: "This is the review that I'm leaving on the tech of your org. It's so back actually it needs an upgrade for real"
+  },
+  {
+    category: "Agriculture",
+    content: "This is the review that I'm leaving on the tech of your org. It's so back actually it needs an upgrade for real"
+  },
+]
+
 const page = () => {
   return (
     <div className='inter flex flex-col gap-6'>
@@ -10,7 +21,9 @@ const page = () => {
         <p className='text-black/60 base'>Review and manage feedbacks submitted to your organisation</p>
       </div>
       <div className='grid grid-cols-3 gap-6'>
-        <FeedbackCard />
+        {feedbacks.map((feedback) => (
+          <FeedbackCard key={feedback.category} feedback={feedback} />
+        ))}
       </div>
     </div>
   )

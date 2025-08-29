9cefdcde5305e91f0957c70324e8decaafe6a2a4
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-29 22:16:10 +0200

    ADDED: Localhost url to test

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `http://localhost:5000/api/:path*`,
       },
     ]
   }

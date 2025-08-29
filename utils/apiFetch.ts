ea0ce6cecc60ae76fcaf40fa5226c4b0773f359c
xCHLOMIx <gutabarwaa@gmail.com>
2025-08-29 22:13:18 +0200

    CHANGED: apiFetch not to use environment variables

--- utils/apiFetch.ts
+++ utils/apiFetch.ts
@@ -1,20 +1,5 @@
 export const apiFetch = async (url: string, options: RequestInit = {}) => {
     const isServer = typeof window === 'undefined';
-    // Use NEXT_PUBLIC_BACKEND_URL for client, BACKEND_URL for server
-    let baseUrl = isServer
-        ? process.env.BACKEND_URL
-        : process.env.NEXT_PUBLIC_BACKEND_URL;
-
-    if (!baseUrl) {
-        throw new Error('Backend URL is not set. Please define NEXT_PUBLIC_BACKEND_URL in your .env file for client-side, and BACKEND_URL for server-side.');
-    }
-
-    // Ensure protocol is present
-    if (!/^https?:\/\//.test(baseUrl)) {
-        baseUrl = 'http://' + baseUrl;
-    }
-
-    url = baseUrl + url;
 
     let response = await fetch(url, {
         ...options,
@@ -26,7 +11,7 @@ export const apiFetch = async (url: string, options: RequestInit = {}) => {
     });
 
     if (response.status === 401) {
-        await fetch(baseUrl + '/api/auth/refresh-token', {
+        await fetch('/api/auth/refresh-token', {
             method: 'POST',
             credentials: 'include',
         });
